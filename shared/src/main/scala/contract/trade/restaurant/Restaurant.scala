32541e45c52de4ea99abbe7c9bc977f53101b1d3
aaron.pritzlaff <aaron@kindservices.co.uk>
2024-05-04 22:06:31 +0100

    introduced App for runnable programs

--- shared/src/main/scala/contract/trade/restaurant/Restaurant.scala
+++ shared/src/main/scala/contract/trade/restaurant/Restaurant.scala
@@ -1,17 +1,35 @@
 package contract.trade.restaurant
 
 import contract.trade.AppRuntime
+import zio.*
+import contract.*
+
+trait Restaurant {
+  def placeOrder(order: Order): Task[OrderId | OrderRejection]
+}
+
+trait App[F[_]] {
+
+  def onInput[A]: F[A] => Task[A]
+
+  private given ~>[F, Task] with {
+    override def apply[A](fa: F[A]): Task[A] = onInput(fa)
+  }
+
+  def run[A](fa: Program[F, A]): Task[A] = fa.foldMap[Task]
+}
 
 object Restaurant {
 
-  import RestaurantLogic.*
-  val app = new AppRuntime[RestaurantLogic]
+  def apply(handle: [A] => RestaurantLogic[A] => Task[A]): Restaurant = new Restaurant
+    with App[RestaurantLogic] {
 
-  val ok = app.register { case MakeDish(dish) =>
-    zio.ZIO.attempt(println("making dish"))
+    override def onInput[A] = handle[A]
+
+    def placeOrder(order: Order): Task[OrderId | OrderRejection] = run(
+      RestaurantLogic.placeOrder(order)
+    )
   }
 
-  def main(args: Array[String]) = {
-    println("hi")
-  }
+  def main(args: Array[String]) = println("hi")
 }
